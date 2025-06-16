72e77a4205270e43b60bd35c09a10609c12711b0
Lucas Barbosa <luc45.2004@gmail.com>
2025-06-16 17:28:25 -0300

    updating for vercel

--- server.js
+++ server.js
@@ -263,6 +263,4 @@ function getAgeRating(rating) {
   return ratings[rating] || 'Desconhecido';
 }
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
+module.exports = app;
